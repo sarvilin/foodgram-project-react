2fbf03f98d0f4bde8e9256de7957ebc74101de6c
Алексей Сарвилин <a.sarvilin@yandex.ru>
2022-09-12 14:52:16 +0300

    deploy5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -264,7 +264,7 @@ function App() {
             />
           </Route>
           <Route path='/'>
-            {loggedIn ? <Redirect to='/recipes' /> : <Redirect to='/signin'/>}
+            {loggedIn ? <Redirect to='/recipes' /> : <Redirect to='/recipes'/>}
           </Route>
         </Switch>
         <Footer />
