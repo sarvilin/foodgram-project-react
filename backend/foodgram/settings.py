9ac021def49c31227694073b41a58797ea676295
Алексей Сарвилин <a.sarvilin@yandex.ru>
2022-09-20 01:01:17 +0300

    ssl4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -2,10 +2,10 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='foo')
 
