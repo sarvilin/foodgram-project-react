2cca740e4209d103b307728034f14b659609d56e
Алексей Сарвилин <a.sarvilin@yandex.ru>
2022-09-20 01:13:37 +0300

    ssl5

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
